6203a8e175573f780eb1ea9df1d127cfdcc6b362
Anis <imarahmandev@gmail.com>
2024-09-19 12:58:16 +0100

    :zap:  scientific notation

--- 11.1.3.py
+++ 11.1.3.py
@@ -5,4 +5,4 @@ radius = float(input("Enter the radius of the sphere: "))
 # area of sphere = 4 * pi * r^2
 area = 4 * pi * radius ** 2
 
-print(f"The area of sphere with radius {radius} is {area:.2f}")
+print(f"The area of sphere with radius {radius} is {"{:2f}".format(area) if area < 1e6 else  "{:e}".format(area)}.")
