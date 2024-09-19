75d6ab14c32a9b27dc30102d6bea9b6f69606ffc
Anis <imarahmandev@gmail.com>
2024-09-19 12:47:31 +0100

    :zap: Area of sphere

---
+++ 11.1.3.py
@@ -0,0 +1,8 @@
+from math import pi
+
+radius = float(input("Enter the radius of the sphere: "))
+
+# area of sphere = 4 * pi * r^2
+area = 4 * pi * radius ** 2
+
+print(f"The area of sphere with radius {radius} is {area:.2f}")
