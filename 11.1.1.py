34afe07d720e9f987da96586528496721ef6a3d5
Anis <imarahmandev@gmail.com>
2024-09-19 12:32:03 +0100

    :zap: Type conversion and checker

---
+++ 11.1.1.py
@@ -0,0 +1,9 @@
+user_input = float(input("Enter a floating number: "))
+
+user_input_str  = str(user_input)
+
+user_input_int = int(user_input)
+
+print(f"Floating number: {user_input}, and type is {type(user_input)}")
+print(f"String number: {user_input_str}, and type is {type(user_input_str)}")
+print(f"Integer number: {user_input_int}, and type is {type(user_input_int)}")
