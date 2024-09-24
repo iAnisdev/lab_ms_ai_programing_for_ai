6e74737e66991be1d4011c383db8639a68febd02
Anis <imarahmandev@gmail.com>
2024-09-24 15:56:50 +0100

    :zap: Str to int convertor

---
+++ week_2/2.1.py
@@ -0,0 +1,11 @@
+def strToIntConvertor(strSet):
+    intValue = int(strSet)
+    print(f"Int value of the string passed is {intValue}")
+    sum = intValue + intValue
+    print(f"Sum of {intValue} passed is {sum}")
+    sub = intValue - 25
+    print(f"Subtraction of 25 from {intValue} passed is {sub}")
+
+strToIntConvertor('392')
+strToIntConvertor('124')
+strToIntConvertor('567')
