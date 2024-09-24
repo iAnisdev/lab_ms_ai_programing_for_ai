62fe327c6c1e99218457f4c86cd35a9cbc5596aa
Anis <imarahmandev@gmail.com>
2024-09-24 15:24:14 +0100

    :zap: move to function structure

--- week_2/1.1.2.py
+++ week_2/1.1.2.py
@@ -1,9 +1,12 @@
 a = float(input("Enter a number: "))
 b = float(input("Enter another number: "))
 
-if a > b:
-    print(f"The first number {a} is greater than the second number {b}.")
-elif a < b:
-    print(f"The second number {b} is greater than the first number {a}.")
-else:
-    print(f"The first number {a} is equal to the second number {b}.")
+def checkGreaterNum(a , b):
+    if a > b:
+        print(f"The first number {a} is greater than the second number {b}.")
+    elif a < b:
+        print(f"The second number {b} is greater than the first number {a}.")
+    else:
+        print(f"The first number {a} is equal to the second number {b}.")
+
+checkGreaterNum(a , b)
