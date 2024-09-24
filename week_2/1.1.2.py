6b3241361f54a6790023e9ab61be2e830df274bf
Anis <imarahmandev@gmail.com>
2024-09-24 15:22:20 +0100

    :zap: Question 2

---
+++ week_2/1.1.2.py
@@ -0,0 +1,9 @@
+a = float(input("Enter a number: "))
+b = float(input("Enter another number: "))
+
+if a > b:
+    print(f"The first number {a} is greater than the second number {b}.")
+elif a < b:
+    print(f"The second number {b} is greater than the first number {a}.")
+else:
+    print(f"The first number {a} is equal to the second number {b}.")
