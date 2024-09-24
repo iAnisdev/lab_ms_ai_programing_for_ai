95d9bb9d85632aa574e3db23b82eea1d83407431
Anis <imarahmandev@gmail.com>
2024-09-24 16:21:27 +0100

    :zap: Number checker

---
+++ week_2/3.2.py
@@ -0,0 +1,14 @@
+def numberChecker(i):
+    if i == 0: 
+        print('Zero')
+    elif i % 2 == 0:
+        print('Even')
+    else:
+        print("Odd")
+    
+
+numberChecker(0)
+numberChecker(4)
+numberChecker(17)
+numberChecker(28)
+numberChecker(33)
