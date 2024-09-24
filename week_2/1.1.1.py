b660f3a4ba02db642f32c22c0f83d532897794fe
Anis <imarahmandev@gmail.com>
2024-09-24 15:21:21 +0100

    :zap: Question 1

---
+++ week_2/1.1.1.py
@@ -0,0 +1,14 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+mod = a % b
+
+print(f"Result of {a} + {b} is : {sum}")
+print(f"Result of {a} - {b} is : {sub}")
+print(f"Result of {a} * {b} is : {mul}")
+print(f"Result of {a} / {b} is : {div}")
+print(f"Result of {a} % {b} is : {mod}")
