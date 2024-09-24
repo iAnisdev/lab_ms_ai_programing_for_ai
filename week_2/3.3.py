78315c0a4bcb8cc03aa3fb1a9a2f200cc9f863b6
Anis <imarahmandev@gmail.com>
2024-09-24 16:22:47 +0100

    :zap: number checker

---
+++ week_2/3.3.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+if a > b:
+    print(f"The first number {a} is greater.")
+elif a < b:
+    print(f"The second number {b} is greater.")
+else:
+    print(f"Both are equal.")
