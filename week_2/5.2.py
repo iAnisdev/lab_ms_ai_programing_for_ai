6b6e2e25928a32266c36b1f31587658e701302db
Anis <imarahmandev@gmail.com>
2024-10-08 13:03:27 +0100

    :white_check_mark: Lambda test

--- week_2/5.2.py
+++ week_2/5.2.py
@@ -1,4 +1,4 @@
 lambda_product = lambda a, b , c : a * b * c
 
-print(lambda_product(2 , 3 , 4))
-print(lambda_product(5.2 , 5.8 , 2.2))
+assert lambda_product(2 , 3 , 4) == 24 , "Should be 24"
+assert lambda_product(5.2 , 5.8 , 2.2) == 66.352 , "Should be 66.352"
