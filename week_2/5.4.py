7e440935fc45d683238f85f16a43a9327bf10c06
Anis <imarahmandev@gmail.com>
2024-10-09 17:49:51 +0100

    :zap: iterative approach

--- week_2/5.4.py
+++ week_2/5.4.py
@@ -3,6 +3,15 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
-    
+
 print(factorial(5))
 print(factorial(10))
+
+def factorial_iterative(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+print(factorial_iterative(5))
+print(factorial_iterative(10))
